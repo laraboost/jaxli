3166eba1a1669eaab530db9a4456a770cb56c147
Vince Mitchell <vincent.mitchell@gmail.com>
2018-02-15 16:09:47 -0500

    vanilla models, migrations, controllers

---
+++ database/migrations/2018_02_15_210846_create_feature_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeatureRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feature_requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feature_requests');
+    }
+}
