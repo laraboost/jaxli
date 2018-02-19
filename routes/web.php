020b3e29b29ff429f0ee57503d44aaea7799fd28
Vince Mitchell <vincent.mitchell@gmail.com>
2018-02-19 06:34:57 -0500

    switch default page to / instead of home

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'FeatureRequestController@index')->name('home');
+Route::get('/', 'FeatureRequestController@index')->name('home');
